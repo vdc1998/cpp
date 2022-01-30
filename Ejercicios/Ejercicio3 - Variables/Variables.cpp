4736c85d2919f6d90004553bfa35002b8a0a4111
Victor Cuellar <vdc1998@gmail.com>
2022-01-30 16:30:13 -0600

    Agregando ejercicio 4 y 5

---
+++ Ejercicios/Ejercicio3 - Variables/Variables.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int numeroEntero;
+    numeroEntero = 15;
+
+    cout << "Entero: " << numeroEntero << endl;
+
+    numeroEntero = 18;
+    cout <<"Entero modificador: "<< numeroEntero;
+    return 0;
+}
