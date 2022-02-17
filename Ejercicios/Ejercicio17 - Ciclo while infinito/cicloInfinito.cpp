a7b7c20fbf8d4674b7c626ace48288e24f859bb3
Victor Cuellar <vdc1998@gmail.com>
2022-02-16 19:12:36 -0600

    agregando ejercicio ciclo infinito

---
+++ Ejercicios/Ejercicio17 - Ciclo while infinito/cicloInfinito.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int ciclos = 0;
+    while(true){
+        cout << "*";
+
+        ciclos = ciclos +1;
+    }
+    return 0;
+}
