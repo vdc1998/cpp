b4a29d04b836e4791d215526d1ebdbb855922671
Victor Cuellar <vdc1998@gmail.com>
2022-02-16 21:10:18 -0600

    Tarea 4

---
+++ Ejercicios/Ejercicio25 - Suma de pares e impares y Total/pares.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int pares = 0;
+    for (int i = 1; i < 10; i++)
+    {
+        if (i % 2 == 0){
+            pares = pares + 1;
+        }
+        cout << i << " ";
+    }
+    
+    cout << endl;
+    cout << endl;
+    cout << "Total pares: "<< pares;
+
+    return 0;
+}
